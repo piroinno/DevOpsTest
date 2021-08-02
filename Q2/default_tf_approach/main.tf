02ec14c6d2d1e757a00003ed0c1443366e1254a3
Peter Irojah <pi@pirolabs.com>
2021-08-02 23:27:21 +0100

    Fixed scope lock on rg, using id instead of name

--- Q2/default_tf_approach/main.tf
+++ Q2/default_tf_approach/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "resource_group" {
 
 resource "azurerm_management_lock" "resource_group_lock" {
   name       = "q2-rg-lock"
-  scope      = azurerm_resource_group.resource_group.name
+  scope      = azurerm_resource_group.resource_group.id
   lock_level = "CanNotDelete"
   notes      = "This Resource Group cannot be deleted"
 }
